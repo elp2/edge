5a5c69eabd174048b5599b451cf28f5f6971fe03
Edward Palmer <edward.palmer@gmail.com>
2019-08-27 22:24:51 -0700

    CP commands.

--- src/BitCommand.cpp
+++ src/BitCommand.cpp
@@ -14,6 +14,16 @@ void andAWithDestination(CPU *cpu, Destination d) {
     cpu->flags.c = false;
 }
 
+void cp(CPU *cpu, Destination d) {
+    uint8_t a = cpu->Get8Bit(Register_A);
+    uint8_t n = cpu->Get8Bit(d);
+
+    cpu->flags.z = (a == n);
+    cpu->flags.n = true;
+    cpu->flags.h = a <= n; // TODO Test a>n A=n, a<n
+    cpu->flags.c = a > n;
+}
+
 void xorAWithDestination(CPU *cpu, Destination d) {
     uint8_t xored = cpu->Get8Bit(Register_A) ^ cpu->Get8Bit(d);
     cpu->Set8Bit(Register_A, xored);
@@ -96,6 +106,35 @@ void BitCommand::Run(CPU *cpu, MMU *mmu) {
         xorAWithDestination(cpu, Eat_PC_Byte);
         return;
 
+    // CPs.
+    case 0xbf:
+        cp(cpu, Register_A);
+        return;
+    case 0xb8:
+        cp(cpu, Register_B);
+        return;
+    case 0xb9:
+        cp(cpu, Register_C);
+        return;
+    case 0xba:
+        cp(cpu, Register_D);
+        return;
+    case 0xbb:
+        cp(cpu, Register_E);
+        return;
+    case 0xbc:
+        cp(cpu, Register_H);
+        return;
+    case 0xbd:
+        cp(cpu, Register_L);
+        return;
+    case 0xbe:
+        cp(cpu, Address_HL);
+        return;
+    case 0xfe:
+        cp(cpu, Eat_PC_Byte);
+        return;
+
     default:
         cout << "Unknown bit command: 0x" << hex << unsigned(opcode);
         assert(false);
@@ -123,4 +162,14 @@ void registerBitCommands(AbstractCommandFactory *factory) {
     factory->RegisterCommand(new BitCommand(0xad, "XOR L", 4));
     factory->RegisterCommand(new BitCommand(0xae, "XOR (HL)", 8));
     factory->RegisterCommand(new BitCommand(0xee, "XOR #", 8));
+
+    factory->RegisterCommand(new BitCommand(0xb8, "CP B", 4));
+    factory->RegisterCommand(new BitCommand(0xb9, "CP C", 4));
+    factory->RegisterCommand(new BitCommand(0xba, "CP D", 4));
+    factory->RegisterCommand(new BitCommand(0xbb, "CP E", 4));
+    factory->RegisterCommand(new BitCommand(0xbc, "CP H", 4));
+    factory->RegisterCommand(new BitCommand(0xbd, "CP L", 4));
+    factory->RegisterCommand(new BitCommand(0xbe, "CP (HL)", 8));
+    factory->RegisterCommand(new BitCommand(0xbf, "CP A", 4));
+    factory->RegisterCommand(new BitCommand(0xfe, "CP #", 8));
 }
