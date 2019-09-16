76a36b20ca41e860fade408cda6ce6897b786d45
Edward Palmer <edward.palmer@gmail.com>
2019-09-15 21:08:07 -0700

    Load nn SP test.

---
+++ tests/load_command_test.cc
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+
+#include "CPU.hpp"
+#include "Utils.hpp"
+
+class LoadCommandTest : public ::testing::Test {
+ protected:
+    LoadCommandTest() {};
+    ~LoadCommandTest() {};
+};
+
+TEST(LoadCommandTest, LDnnSP) {
+    const uint8_t LDnnSP = 0x08;
+    CPU *cpu = getTestingCPUWithInstructions(vector<uint8_t>{ LDnnSP, 0x00, 0xC1 });
+    uint32_t cycles = cpu->cycles();
+    uint16_t expected_sp = 0xFFF8;
+    cpu->Set16Bit(Register_SP, expected_sp);
+
+    cpu->Step();
+    cpu->Set16Bit(Register_HL, 0xC100);
+    ASSERT_EQ(cpu->Get8Bit(Address_HL), 0xF8);
+    cpu->Set16Bit(Register_HL, 0xC101);
+    ASSERT_EQ(cpu->Get8Bit(Address_HL), 0xFF);
+}
