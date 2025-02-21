3c7997d2a23526f5d60cbb6b9e8f2c4c5efb8b52
Edward Palmer <891364+elp2@users.noreply.github.com>
2025-02-20 20:31:29 -0800

    Load app rom from command line.

--- src/main.cc
+++ src/main.cc
@@ -7,27 +7,12 @@
 int main(int argc, char* argv[]) {
   (void)argc;
   (void)argv;
+  std::cout << "Loading ROM " << argv[1] << std::endl;
+
   std::ofstream log("log.txt");
   std::cout.rdbuf(log.rdbuf());
 
-  // System *system = new System("../../dmg-acid2.gb");
-  // System *system = new System("../../cgb_sound.gb");
-
-  // System *system = new
-  // System("../../gb-test-roms/instr_timing/instr_timing.gb"); System *system =
-  // new System("../../gb-test-roms/cpu_instrs/cpu_instrs.gb"); // All OK, #3
-  // failed. System *system = new
-  // System("../../gb-test-roms/instr_timing/instr_timing.gb"); // Passes.
-  // System *system = new
-  // System("../../gb-test-roms/interrupt_time/interrupt_time.gb");  // Fails.
-  // CGB Only? System *system = new
-  // System("../../gb-test-roms/mem_timing/mem_timing.gb");  // Fails 1, 2, 3.
-
-  System* system = new System(
-      "../../gb-test-roms/cpu_instrs/individual/03-op sp,hl.gb");  // E8 F8
-                                                                   // Failed
-                                                                   // (Add, ld
-                                                                   // sp).
+  System *system = new System(argv[1]);
   system->Main();
   return 0;
 }
