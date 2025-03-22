f621285f79392e582fa4953f5b24560ae16ac453
Edward Palmer <891364+elp2@users.noreply.github.com>
2025-03-22 08:02:18 -0600

    Fix argv handling and error messages.

--- src/main.cc
+++ src/main.cc
@@ -5,21 +5,22 @@
 
 int main(int argc, char* argv[]) {
   std::string state_dir = "";
-  if (argc < 1) {
+  if (argc < 2) {
     std::cout << "Usage: rom.gb [States/statename]" << std::endl;
     return 0;
-  } else if (argc < 2) {
+  } else if (argc == 2) {
     state_dir = "";
   } else {
     state_dir = argv[2];
   }
 
-  std::cout << "Loading ROM " << argv[1] << std::endl;
+  std::string rom_file = argv[1];
+  std::cout << "Loading ROM " << rom_file << std::endl;
 
   std::ofstream log("log.txt");
   std::cout.rdbuf(log.rdbuf());
 
-  System *system = new System(argv[1], state_dir);
+  System *system = new System(rom_file, state_dir);
   system->Main();
   return 0;
 }
